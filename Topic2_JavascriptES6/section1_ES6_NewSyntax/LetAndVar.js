e1b959e59610d7903e88c806ceab8ee0b8d7494f
Manh <manuhy04@gmail.com>
2023-09-19 18:08:07 +0700

    section_1 ES^

---
+++ Topic2_JavascriptES6/section1_ES6_NewSyntax/LetAndVar.js
@@ -0,0 +1,25 @@
+// Using range
+// for (var i = 10; i > 0; i--) {
+//     for (let j = i; j > 0; j--) {
+//         console.log('*')
+//     }
+//     console.log('\n')
+// }
+// console.log('var: ', i)
+// console.log('let: ', j)
+
+// convert
+// var counter = 10;
+// console.log(window.counter);
+
+// let counter1 = 20;
+// console.log(window.counter1);
+
+// re-init
+// var x = 10;
+// var x = 20;
+// console.log(x);
+
+// let y = 1;
+// y = 4;
+// console.log(y);
